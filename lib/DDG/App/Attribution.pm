3c6f18b34abfbdf7ca0a6d1c01190ad99b255a5e
jimatcx <84412582+jimatcx@users.noreply.github.com>
2023-04-14 14:47:09 -0500

    Update Attribution.pm

--- lib/DDG/App/Attribution.pm
+++ lib/DDG/App/Attribution.pm
@@ -32,6 +32,8 @@ sub BUILD {
 				print "\nNo attributions for ".$_."\n\n";
 				print "\nAdding another Jimbeaux abc attribution vuln for".$_."\n\n";
 				print "\nAdding another Jimbeaux 808 bass attribution vuln for".$_."\n\n";
+				print "\nAdding another Jimbeaux def attribution vuln for".$_."\n\n";
+				print "\nAdding another Jimbeaux 666 bass attribution vuln for".$_."\n\n";
 			}
 		}
 	}
